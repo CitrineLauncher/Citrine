b7a00c91e49d436b3f2a9f8678e3b36af9b58ee3
Clyraz <111095996+Clyraz@users.noreply.github.com>
2026-08-03 06:15:42 +0200

    Add WUProtocolTemplates

--- Citrine/PreBuild.fsx
+++ Citrine/PreBuild.fsx
@@ -2,6 +2,7 @@ open System
 open System.IO
 open System.Collections.Generic
 open System.Text
+open System.Security.Cryptography
 
 type XboxCikEntry(bytes: byte[]) =
     member val Id = new Guid(bytes.[..0xF])
@@ -62,6 +63,58 @@ let GenerateXboxCikEntriesHeader () =
 
     headerBuilder.ToString()
 
+let GenerateWUProtocolTemplatesHeader () =
+    
+    let headerBuilder = new StringBuilder(2048)
+    headerBuilder
+        .AppendLine("#pragma once")
+        .AppendLine()
+        .AppendLine("namespace Citrine::Windows {")
+        .AppendLine()
+        .AppendLine("\tstruct WUProtocolTemplates {")
+        |> ignore
+
+    let templateDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Windows/WUProtocol")
+
+    try
+        for templateFile in Directory.GetFiles(templateDirectory, "*.xml", SearchOption.TopDirectoryOnly) do
+
+            let templateName = Path.GetFileNameWithoutExtension(templateFile)
+            let templateContent = File.ReadAllText(templateFile)
+
+            headerBuilder
+                .AppendLine()
+                .AppendLine($"\t\tstatic constexpr auto& {templateName}{{")
+                .AppendLine($"R\"({templateContent})\"")
+                .AppendLine("\t\t};")
+                |> ignore
+
+    with
+        | :? DirectoryNotFoundException -> ()
+        | ex -> raise(ex)
+
+    headerBuilder
+        .AppendLine("\t};")
+        .AppendLine("}")
+        |> ignore
+
+    headerBuilder.ToString()
+
+let WriteHeaderFile (path: string, content: string) =
+    
+    let contentBytes = Encoding.UTF8.GetBytes(content);
+    use fileStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite)
+    
+    use sha256 = SHA256.Create()
+    let oldContentHash = sha256.ComputeHash(fileStream);
+    let newContentHash = sha256.ComputeHash(contentBytes);
+
+    if oldContentHash <> newContentHash then
+        fileStream.Seek(0, SeekOrigin.Begin)        |> ignore
+        fileStream.Write(contentBytes)              |> ignore
+        fileStream.SetLength(contentBytes.Length)   |> ignore
+
 let generatedFilesDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Generated Files");
 Directory.CreateDirectory(generatedFilesDirectory);
-File.WriteAllText(Path.Combine(generatedFilesDirectory, "XboxCikEntries.h"), GenerateXboxCikEntriesHeader())
+WriteHeaderFile(Path.Combine(generatedFilesDirectory, "XboxCikEntries.h"), GenerateXboxCikEntriesHeader())
+WriteHeaderFile(Path.Combine(generatedFilesDirectory, "WUProtocolTemplates.h"), GenerateWUProtocolTemplatesHeader())
