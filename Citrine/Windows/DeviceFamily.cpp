58a2be1a3b12b131473552354cea1d7fff9deae3
Clyraz <111095996+Clyraz@users.noreply.github.com>
2026-08-20 05:11:27 +0200

    Add DeviceFamily enum

--- Citrine/Windows/DeviceFamily.cpp
+++ Citrine/Windows/DeviceFamily.cpp
@@ -2,6 +2,8 @@
 #include "DeviceFamily.h"
 
 #include "Core/Util/ParseInteger.h"
+#include "Core/Util/Ascii.h"
+#include "Core/Util/Frozen.h"
 
 #include <winrt/Windows.System.Profile.h>
 
@@ -11,6 +13,76 @@ namespace winrt {
 }
 
 namespace Citrine::Windows {
+
+    DeviceFamily::DeviceFamily(std::string_view name) noexcept {
+
+        struct NameComparer {
+
+            static constexpr auto operator()(std::string_view left, std::string_view right) noexcept -> bool {
+
+                constexpr auto toLower = [](char ch) static { return Ascii::ToLower(ch); };
+
+                return std::ranges::lexicographical_compare(left, right, {}, toLower, toLower);
+            }
+        };
+
+        static constexpr auto knownDeviceFamilies = MakeFrozenMap<std::string_view, DeviceFamily, NameComparer>({
+
+			{ "Windows.Universal", Universal },
+			{ "Windows.Windows8x", Windows8x },
+			{ "Windows.WindowsPhone8x", WindowsPhone8x },
+			{ "Windows.Desktop", Desktop },
+			{ "Windows.Mobile", Mobile },
+			{ "Windows.Xbox", Xbox },
+			{ "Windows.Team", Team },
+			{ "Windows.IoT", IoT },
+			{ "Windows.IoTHeadless", IoTHeadless },
+			{ "Windows.Server", Server },
+			{ "Windows.Holographic", Holographic },
+			{ "Windows.XBoxSRA", XBoxSRA },
+			{ "Windows.XBoxERA", XBoxERA },
+			{ "Windows.ServerNano", ServerNano },
+			{ "Windows.8828080", _8828080 },
+			{ "Windows.7067329", _7067329 },
+			{ "Windows.Core", Core },
+			{ "Windows.CoreHeadless", CoreHeadless }
+        });
+
+        auto it = knownDeviceFamilies.find(name);
+        if (it != knownDeviceFamilies.end())
+            value = it->second;
+    }
+
+    auto DeviceFamily::Name() const noexcept -> std::string_view {
+
+        auto name = std::string_view{ "<unknown>" };
+        switch (value) {
+		case Universal:         name = "Windows.Universal";         break;
+		case Windows8x:         name = "Windows.Windows8x";         break;
+		case WindowsPhone8x:    name = "Windows.WindowsPhone8x";    break;
+		case Desktop:           name = "Windows.Desktop";           break;
+		case Mobile:            name = "Windows.Mobile";            break;
+		case Xbox:              name = "Windows.Xbox";              break;
+		case Team:              name = "Windows.Team";              break;
+		case IoT:               name = "Windows.IoT";               break;
+		case IoTHeadless:       name = "Windows.IoTHeadless";       break;
+		case Server:            name = "Windows.Server";            break;
+		case Holographic:       name = "Windows.Holographic";       break;
+		case XBoxSRA:           name = "Windows.XBoxSRA";           break;
+		case XBoxERA:           name = "Windows.XBoxERA";           break;
+		case ServerNano:        name = "Windows.ServerNano";        break;
+		case _8828080:          name = "Windows.8828080";           break;
+		case _7067329:          name = "Windows.7067329";           break;
+		case Core:              name = "Windows.Core";              break;
+		case CoreHeadless:      name = "Windows.CoreHeadless";      break;
+        }
+        return name;
+    }
+
+    DeviceFamily::operator std::string() const {
+
+        return std::string{ Name() };
+    }
 
 	auto DeviceFamilyVersion::FromInteger(std::uint64_t versionNumber) noexcept -> DeviceFamilyVersion {
 
@@ -43,7 +115,7 @@ namespace Citrine::Windows {
 
 			return DeviceFamilyInfo{
 
-				.DeviceFamily = winrt::to_string(versionInfo.DeviceFamily()),
+				.DeviceFamily = Windows::DeviceFamily{ winrt::to_string(versionInfo.DeviceFamily()) },
 				.DeviceFamilyVersion = DeviceFamilyVersion::FromInteger(ParseInteger<std::uint64_t>(versionInfo.DeviceFamilyVersion()).value_or({})),
 				.DeviceForm = winrt::to_string(deviceForm)
 			};
--- Citrine/Windows/DeviceFamily.h
+++ Citrine/Windows/DeviceFamily.h
@@ -4,6 +4,85 @@
 
 namespace Citrine::Windows {
 
+    class DeviceFamily {
+    public:
+
+        enum struct ValueType : std::uint32_t {};
+
+        static const DeviceFamily Unknown;
+        static const DeviceFamily Universal;
+        static const DeviceFamily Windows8x;
+        static const DeviceFamily WindowsPhone8x;
+        static const DeviceFamily Desktop;
+        static const DeviceFamily Mobile;
+        static const DeviceFamily Xbox;
+        static const DeviceFamily Team;
+        static const DeviceFamily IoT;
+        static const DeviceFamily IoTHeadless;
+        static const DeviceFamily Server;
+        static const DeviceFamily Holographic;
+        static const DeviceFamily XBoxSRA;
+        static const DeviceFamily XBoxERA;
+        static const DeviceFamily ServerNano;
+        static const DeviceFamily _8828080;
+        static const DeviceFamily _7067329;
+        static const DeviceFamily Core;
+        static const DeviceFamily CoreHeadless;
+
+        constexpr DeviceFamily() noexcept = default;
+
+        explicit constexpr DeviceFamily(std::underlying_type_t<ValueType> value) noexcept
+
+            : value{ value }
+        {}
+
+        explicit DeviceFamily(std::string_view name) noexcept;
+
+        constexpr DeviceFamily(DeviceFamily const&) noexcept = default;
+        constexpr auto operator=(DeviceFamily const&) noexcept -> DeviceFamily& = default;
+
+        auto Name() const noexcept -> std::string_view;
+
+        explicit operator std::string() const;
+
+        constexpr operator ValueType() const noexcept {
+
+            return value;
+        }
+
+        template<std::integral T>
+        explicit constexpr operator T() const noexcept {
+
+            return static_cast<T>(value);
+        }
+
+        constexpr auto operator<=>(DeviceFamily const&) const noexcept -> std::strong_ordering = default;
+
+    private:
+
+        ValueType value{ 0xFFFFFFFF };
+    };
+
+    inline constexpr DeviceFamily DeviceFamily::Unknown         { 0xFFFFFFFF };
+    inline constexpr DeviceFamily DeviceFamily::Universal       { 0 };
+    inline constexpr DeviceFamily DeviceFamily::Windows8x       { 1 };
+    inline constexpr DeviceFamily DeviceFamily::WindowsPhone8x  { 2 };
+    inline constexpr DeviceFamily DeviceFamily::Desktop         { 3 };
+    inline constexpr DeviceFamily DeviceFamily::Mobile          { 4 };
+    inline constexpr DeviceFamily DeviceFamily::Xbox            { 5 };
+    inline constexpr DeviceFamily DeviceFamily::Team            { 6 };
+    inline constexpr DeviceFamily DeviceFamily::IoT             { 7 };
+    inline constexpr DeviceFamily DeviceFamily::IoTHeadless     { 8 };
+    inline constexpr DeviceFamily DeviceFamily::Server          { 9 };
+    inline constexpr DeviceFamily DeviceFamily::Holographic     { 10 };
+    inline constexpr DeviceFamily DeviceFamily::XBoxSRA         { 11 };
+    inline constexpr DeviceFamily DeviceFamily::XBoxERA         { 12 };
+    inline constexpr DeviceFamily DeviceFamily::ServerNano      { 13 };
+    inline constexpr DeviceFamily DeviceFamily::_8828080        { 14 };
+    inline constexpr DeviceFamily DeviceFamily::_7067329        { 15 };
+    inline constexpr DeviceFamily DeviceFamily::Core            { 16 };
+    inline constexpr DeviceFamily DeviceFamily::CoreHeadless    { 17 };
+
     struct DeviceFamilyVersion : VersionNumberBase<DeviceFamilyVersion, 4> {
 
         static auto FromInteger(std::uint64_t versionNumber) noexcept -> DeviceFamilyVersion;
@@ -35,7 +114,7 @@ namespace Citrine::Windows {
 
         static auto Get() -> DeviceFamilyInfo const&;
 
-        std::string DeviceFamily;
+        DeviceFamily DeviceFamily;
         DeviceFamilyVersion DeviceFamilyVersion;
         std::string DeviceForm;
     };
