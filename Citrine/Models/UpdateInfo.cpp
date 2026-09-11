f894b6c4f00dc6cd7f7498b834810cc2bcec4515
Clyraz <111095996+Clyraz@users.noreply.github.com>
2026-09-11 18:24:10 +0200

    Add UpdateInfo

---
+++ Citrine/Models/UpdateInfo.cpp
@@ -0,0 +1,68 @@
+﻿#include "pch.h"
+#include "UpdateInfo.h"
+#if __has_include("UpdateInfo.g.cpp")
+#include "UpdateInfo.g.cpp"
+#endif
+
+#include "Core/Util/TrivialArray.h"
+
+using namespace Citrine;
+
+namespace winrt {
+
+    using namespace Windows::Foundation;
+}
+
+namespace winrt::Citrine::implementation
+{
+    UpdateInfo::UpdateInfo(::Citrine::ReleaseInfo releaseInfo, winrt::Windows::Foundation::Uri releaseUrl)
+
+        : releaseInfo(std::move(releaseInfo))
+        , releaseUrl(std::move(releaseUrl))
+    {
+        auto& versionNumber = this->releaseInfo.Version;
+        auto buffer = TrivialArray<wchar_t, VersionNumberFormatter::MaxFormattedSize(versionNumber)>{};
+        version = std::wstring_view{ buffer.data(), VersionNumberFormatter::FormatTo(buffer.data(), versionNumber) };
+
+        releaseTag = winrt::to_hstring(this->releaseInfo.Tag);
+    }
+
+    auto UpdateInfo::Version() const noexcept -> winrt::hstring {
+
+        return version;
+    }
+
+    auto UpdateInfo::ReleaseChannel() const noexcept -> winrt::hstring {
+
+        struct Strings {
+
+            winrt::hstring Stable = L"Stable";
+            winrt::hstring Preview = L"Preview";
+        };
+        static auto const strings = Strings{};
+
+        using enum ReleaseChannel;
+
+        auto releaseChannel = winrt::hstring{};
+        switch (releaseInfo.Channel) {
+        case Stable:    releaseChannel = strings.Stable;    break;
+        case Preview:   releaseChannel = strings.Preview;   break;
+        }
+        return releaseChannel;
+    }
+
+    auto UpdateInfo::ReleaseTag() const noexcept -> winrt::hstring {
+
+        return releaseTag;
+    }
+
+    auto UpdateInfo::ReleaseUrl() const noexcept -> winrt::Uri {
+
+        return releaseUrl;
+    }
+
+    auto UpdateInfo::ReleaseInfo() const noexcept -> ::Citrine::ReleaseInfo const& {
+
+        return releaseInfo;
+    }
+}
---
+++ Citrine/Models/UpdateInfo.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "UpdateInfo.g.h"
+
+#include "ReleaseInfo.h"
+
+namespace winrt::Citrine::implementation
+{
+    struct UpdateInfo : UpdateInfoT<UpdateInfo>
+    {
+        UpdateInfo(::Citrine::ReleaseInfo releaseInfo, winrt::Windows::Foundation::Uri releaseUrl);
+
+        auto Version() const noexcept -> winrt::hstring;
+        auto ReleaseChannel() const noexcept -> winrt::hstring;
+        auto ReleaseTag() const noexcept -> winrt::hstring;
+        auto ReleaseUrl() const noexcept -> winrt::Windows::Foundation::Uri;
+
+        auto ReleaseInfo() const noexcept -> ::Citrine::ReleaseInfo const&;
+
+    private:
+
+        ::Citrine::ReleaseInfo releaseInfo;
+        winrt::hstring version;
+        winrt::hstring releaseTag;
+        winrt::Windows::Foundation::Uri releaseUrl;
+    };
+}
+
+namespace winrt::Citrine::factory_implementation
+{
+    /*
+    struct UpdateInfo : UpdateInfoT<UpdateInfo, implementation::UpdateInfo>
+    {
+    };
+    */  
+}
